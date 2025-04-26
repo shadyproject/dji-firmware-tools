886155087c08b571b79e57b9f6b096f4e72ac886
SecurityGuy <securityguy@users.noreply.github.com>
2025-04-26 16:35:29 -0400

    comm_dissector: README for Wireshark 4.4, absolute paths in init.lua
    The patch includes 3 changes:
    
    * Updates comm_dissector/README.md with an installation procedure that works with the current version of Wireshark
    * Corrects capitalization of "LUA" -- the name of the language is "Lua"
    * Updates `init.lua` to default to the current behaviour but easily allow users to specify an absolute path the the rest of the `*.lua` files as recommended in the updated README

--- comm_dissector/wireshark/init.lua
+++ comm_dissector/wireshark/init.lua
@@ -1,15 +1,30 @@
 disable_lua = false
 
-dofile('dji-dumlv1-proto.lua')
+-- Change dji_script_path to point to the directory where you have copied
+-- the Lua scripts. Do not place them in the Wireshark Lua directory.
+-- Please see the README file for more information.
+-- This path should be an absolute path and end with a "/"
+--
+-- Linux/MacOS example:
+--   local dji_script_path = "/path/to/scripts/"
+-- Windows example:
+--   local dji_script_path = "C:\\path\\to\\scripts\\"
 
-dofile('dji-p3-flyrec-proto.lua')
-dofile('dji-p3-batt-proto.lua')
-dofile('dji-p3.lua')
+local dji_script_path = ""
 
-dofile('dji-mavic-flyrec-proto.lua')
-dofile('dji-mavic.lua')
+-- If you have not changed the line above, Lua will look for the scripts
+-- in the current working directory and probably not find them.
 
-dofile('dji-spark-flyrec-proto.lua')
-dofile('dji-spark.lua')
+dofile(dji_script_path .. 'dji-dumlv1-proto.lua')
 
-dofile('dji-write-kml.lua')
+dofile(dji_script_path .. 'dji-p3-flyrec-proto.lua')
+dofile(dji_script_path .. 'dji-p3-batt-proto.lua')
+dofile(dji_script_path .. 'dji-p3.lua')
+
+dofile(dji_script_path .. 'dji-mavic-flyrec-proto.lua')
+dofile(dji_script_path .. 'dji-mavic.lua')
+
+dofile(dji_script_path .. 'dji-spark-flyrec-proto.lua')
+dofile(dji_script_path .. 'dji-spark.lua')
+
+dofile(dji_script_path .. 'dji-write-kml.lua')
